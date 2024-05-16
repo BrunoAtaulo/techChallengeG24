ad9cc9ee39e38c322b57c49ba07f51cf6fc2933c
gft.franciele <gft.franciele@grupofleury.com.br>
2024-05-16 12:16:45 -0300

    contrato post/produtos

--- Domain/Entities/Input/PostProdutos.cs
+++ Domain/Entities/Input/PostProdutos.cs
@@ -1,3 +1,4 @@
+using Domain.Base;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -5,26 +6,18 @@ namespace Domain.Entities.Input
 {
     public class PostProdutos : IValidatableObject
     {
-        public int IdProduto { get; set; }
+  
         public string NomeProduto { get; set; }
-        public decimal ValorProduto { get; set; }
         public string DescricaoProduto { get; set; }
-        public string ImagemProduto { get; set; }
+        public decimal ValorProduto { get; set; }
         public int QuantidadeProduto { get; set; }
-        public int IdCategoria { get; set; }
-        public virtual Categorias Categoria { get; set; }
-
+        public EnumCategoria? NomeCategoria { get; set; }
+     
+   
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (IdProduto <= 0)
-            {
-                yield return new ValidationResult(
-                    "O id do produto é obrigatório e deve ser maior que zero.",
-                    new[] { nameof(IdProduto) }
-                );
-            }
-
+           
             if (string.IsNullOrWhiteSpace(NomeProduto))
             {
                 yield return new ValidationResult("Nome do produto é obrigatório", new string[] { "NomeProduto" });
@@ -35,10 +28,31 @@ namespace Domain.Entities.Input
                 yield return new ValidationResult("Descricao do produto é obrigatório", new string[] { "DescricaoProduto" });
             }
 
-            if (string.IsNullOrWhiteSpace(ImagemProduto))
+            if (ValorProduto <= 0)
             {
-                yield return new ValidationResult("Imagem do produto é obrigatório", new string[] { "ImagemProduto" });
+                yield return new ValidationResult(
+                    "O valor do produto é obrigatório.",
+                     new string[] { "ValorProduto" }
+                );
             }
+
+
+            if (QuantidadeProduto <= 0)
+            {
+                yield return new ValidationResult(
+                    "A quantidade do produto é obrigatória.",
+                     new string[] { "QuantidadeProduto" }
+                );
+            }
+
+            if (!NomeCategoria.HasValue)
+            {
+                yield return new ValidationResult(
+                    "O nome da categoria é obrigatório.",
+                    new[] { nameof(PedidoStatus) }
+                );
+            }
+
         }
     }
 }
